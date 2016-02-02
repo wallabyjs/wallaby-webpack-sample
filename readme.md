2c68811f9120ffccf8e6c087d6d21141fffb1a1c
Artem Govorov <artem.govorov@gmail.com>
2016-02-02 16:52:50 +1000

    Updated the sample

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 ![screen shot 2015-04-13 at 2 51 21 pm](https://cloud.githubusercontent.com/assets/979966/7109975/97b2371a-e1ec-11e4-948e-28af9907f7dd.png)
 
-The sample is using [wallaby-webpack postprocessor](https://github.com/jeffling/wallaby-webpack) and ES6 via babel preprocessor.
+The sample is using [wallaby-webpack postprocessor](https://github.com/jeffling/wallaby-webpack) and ES6 via babel compiler.
--- wallaby.js
+++ wallaby.js
@@ -1,23 +1,21 @@
 'use strict';
 
-var babel = require('babel-core');
 var wallabyWebpack = require('wallaby-webpack');
-
 var webpackPostprocessor = wallabyWebpack({});
 
 module.exports = function (wallaby) {
 
   return {
     files: [
-      { pattern: 'src/*.js', load: false }
+      { pattern: 'src/**/*.js', load: false }
     ],
 
     tests: [
-      { pattern: 'test/*Spec.js', load: false }
+      { pattern: 'test/**/*Spec.js', load: false }
     ],
 
     compilers: {
-      '**/*.js': wallaby.compilers.babel({ babel: babel, presets: ['es2015'] })
+      '**/*.js': wallaby.compilers.babel()
     },
 
     postprocessor: webpackPostprocessor,
